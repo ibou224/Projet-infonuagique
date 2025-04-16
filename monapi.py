bc8578fcafa70d7a77f1c4d1d2d241f2dbeb6b5e
ibou224 <88585134+ibou224@users.noreply.github.com>
2025-04-15 22:04:12 -0400

    Update monapi.py

--- monapi.py
+++ monapi.py
@@ -2,20 +2,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import pandas as pd
-
 from fastapi.middleware.cors import CORSMiddleware
+
+# Initialiser l'application FastAPI
+app = FastAPI()
+
+# Ajouter le middleware CORS après avoir initialisé FastAPI
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # Remplace "*" par l'URL de ton frontend si nécessaire
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 # Charger le modèle sauvegardé
 model = joblib.load("random_forest_model.pkl")
-
-# Initialiser l'application FastAPI
-app = FastAPI()
 
 # Définir le modèle de données pour la requête
 class PredictionRequest(BaseModel):
@@ -33,20 +34,17 @@ def predict(request: PredictionRequest):
     input_dict = request.dict()
     type_payment = input_dict.pop("type_payment")
 
-# Convert type_payment into binary columns
+    # Convert type_payment into binary columns
     input_dict["type_CASH_OUT"] = int(type_payment == "CASH_OUT")
     input_dict["type_DEBIT"] = int(type_payment == "DEBIT")
     input_dict["type_PAYMENT"] = int(type_payment == "PAYMENT")
     input_dict["type_TRANSFER"] = int(type_payment == "TRANSFER")
-    print(input_dict)
 
-# Convert to DataFrame
+    # Convert to DataFrame
     input_data = pd.DataFrame([input_dict])
+
     # Faire une prédiction
     prediction = model.predict(input_data)
 
     # Retourner la prédiction
     return {"prediction": int(prediction[0])}
-
-# Pour exécuter l'application, utilisez la commande suivante dans le terminal :
-# py -m uvicorn monapi:app --reload
